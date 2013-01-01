b75540c17b7a055c4519e10863166522026d750c
Sean Hammond <seanhammond@lavabit.com>
2013-01-01 20:04:26 +0000

    Correct unix -> UNIX

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     scripts=["bin/terminal_velocity"],
     url="https://github.com/seanh/terminal_velocity",
     license="GNU General Public License, Version 3",
-    description="A fast note-taking app for the unix terminal",
+    description="A fast note-taking app for the UNIX terminal",
     long_description=open("README.markdown").read(),
     install_requires=[
         "urwid==1.1.1",
