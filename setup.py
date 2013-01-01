0e2cfc8224d3adfbdb14f45b416d6f65aa21f3a3
Sean Hammond <seanhammond@lavabit.com>
2013-01-01 18:09:47 +0000

    Prepare for 0.1a1 package release
    Not uploaded tp pypi yet, but prepared the repo layout etc for the release

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="Terminal Velocity",
+    version="0.1a1",
+    author="Sean Hammond",
+    packages=["terminal_velocity"],
+    scripts=["bin/terminal_velocity"],
+    url="https://github.com/seanh/terminal_velocity",
+    license="GNU General Public License, Version 3",
+    description="A fast note-taking app for the unix terminal",
+    long_description=open("README.markdown").read(),
+    install_requires=[
+        "urwid==1.1.1",
+        ]
+)
