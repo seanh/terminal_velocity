4ab814ad4c3346f39563d4a7312c87c4125f327d
Sean Hammond <seanhammond@lavabit.com>
2013-01-02 02:08:31 +0000

    Release 0.1a2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="terminal_velocity",
-    version="0.1a1",
+    version="0.1a2",
     author="Sean Hammond",
     packages=["terminal_velocity"],
     scripts=["bin/terminal_velocity"],
