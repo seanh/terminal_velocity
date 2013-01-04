401f0cedced408bcdd951abd88059ec65cb56298
Sean Hammond <seanhammond@lavabit.com>
2013-01-04 14:22:52 +0000

    Version no bump, and add pypi long description

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name="terminal_velocity",
-    version="0.1a4",
+    version="0.1a5",
     author="Sean Hammond",
     packages=["terminal_velocity"],
     scripts=["bin/terminal_velocity"],
     url="http://seanh.github.com/terminal_velocity/",
     license="GNU General Public License, Version 3",
     description="A fast note-taking app for the UNIX terminal",
+    long_description=open("README.rst").read(),
     install_requires=[
         "urwid==1.1.1",
         "chardet==2.1.1",
