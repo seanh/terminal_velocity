76941b9c27bf297700ade2f133d9abcbc124e503
Sean Hammond <seanhammond@lavabit.com>
2013-01-04 14:19:37 +0000

    Update version number and changelog for pypi release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="terminal_velocity",
-    version="0.1a3",
+    version="0.1a4",
     author="Sean Hammond",
     packages=["terminal_velocity"],
     scripts=["bin/terminal_velocity"],
