0af517aa86c4836323c7878cd4f4e33dc4a457ed
Sean Hammond <git@seanh.cc>
2014-10-19 16:18:06 +0200

    Add more programming language version classifiers

--- setup.py
+++ setup.py
@@ -15,6 +15,9 @@ setup(
         "chardet==2.1.1",
         ],
     classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2 :: Only',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         ],
 )
