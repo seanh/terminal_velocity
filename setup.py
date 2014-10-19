4a7eeec07ed69066650ddd6e447277e45938b515
Sean Hammond <git@seanh.cc>
2014-10-19 16:13:30 +0200

    Bump version  number to 0.1.8.
    Also changes version number style to one that works with pip.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="terminal_velocity",
-    version="0.1a7",
+    version="0.1.8",
     author="Sean Hammond",
     packages=["terminal_velocity"],
     scripts=["bin/terminal_velocity"],
